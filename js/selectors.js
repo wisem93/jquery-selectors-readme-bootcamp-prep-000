ddb5ddc748a45ddec5074da8efcfb11836ba6f6a
Marcus Wiseman <marcus.e.wiseman@gmail.com>
2019-05-09 02:22:09 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,20 +1,20 @@
 // declare your functions here...
 function paragraphSelector() {
-  $('p');
+ return $('p');
 }
 
 function lastImageSelector() {
-  $('img:last');
+  return $('img:last');
 }
 
 function ninjaBabySelector() {
-  $('#baby-ninja');
+  return $('#baby-ninja');
 }
 
 function divSelector() {
-  $('.pics');
+  return $('.pics');
 }
 
 function firstListItem() {
-  $('ul:first-child');
+  return $('ul:first-child');
 }
